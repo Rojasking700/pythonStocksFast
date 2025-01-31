93725dc8f9b693add095447b4fb7410889c42165
Rojasking700 <grojaspiano@gmail.com>
2025-01-31 09:00:06 -0600

    tests

--- app/crud/stocks.py
+++ app/crud/stocks.py
@@ -1,5 +1,10 @@
 import pdfplumber
+from groq import Groq
+import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env
+load_dotenv()
 
 def anaylzePDFTest():
   print('this is runingnngngngn')
@@ -21,7 +26,42 @@ def anaylzePDFTest():
       
 
 
+      client = Groq(api_key=os.getenv("GROQ_API_KEY"),)
+      completion = client.chat.completions.create(
+          model="llama-3.1-8b-instant",
+          messages=[
+            {
+              "role": "system",
+              "content": "you are a stock market analysist"
+            },
+
+            # Set a user message for the assistant to respond to.
+            {
+              "role": "user",
+              "content": "summarize this for me:",
+            }, 
+            
+            {
+              "role": "user",
+              "content": pageTextResults[0],
+            }
+            #  "summarize This:",
+            #  pageTextResults[0]
+          ],
+          temperature=1,
+          max_completion_tokens=1024,
+          top_p=1,
+          stream=True,
+          stop=None,
+      )
+
+      for chunk in completion:
+          print(chunk.choices[0].delta.content or "", end="")
+
+
+
   return {
     'extractedText': pageTextResults,
+    'completion': completion
     # 'extractedTables': extractedTables
     }
